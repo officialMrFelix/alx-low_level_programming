6b97909d307d02489d1da65f9907297430d642d1
officialMrFelix <felixobianozie@hotmail.com>
2022-06-10 21:41:23 +0100

    10-print_triangle_alternative.c included

---
+++ 0x04-more_functions_nested_loops/10-print_triangle_alternative.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	if (size <= 0)	/*Print only new line when size is zero or less*/
+	{
+		_putchar('\n');
+	} else	/*Proceed to draw triangle if size is greater than zero*/
+	{
+		int hash;	/*@hash: number of hash characters to print*/
+		int space;	/*@space: number of hash characters to print*/
+		int i;		/*@i: for-loop iterator*/
+		int j;		/*@j: for-loop iterator*/
+
+		for (i = 1; i <= size; i++)	/*Loop for @size number of times*/
+		{
+			space = size - i;
+			hash = size - space;
+
+			for (j = 1; j <= space; j++)	/*Print spaces in @space numb of times*/
+			{
+				_putchar(' ');
+			}
+
+			for (j = 1; j <= hash; j++)	/*Print hashes in @hash numb of times*/
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');	/*Print new line*/
+		}
+	}
+}
